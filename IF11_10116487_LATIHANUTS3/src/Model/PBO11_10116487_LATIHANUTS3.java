54650b06a20e647136f2baf332e23df2c4ec87ad
huldan <32485190+huldan@users.noreply.github.com>
2017-11-14 21:47:25 +0700

    Add files via upload

---
+++ IF11_10116487_LATIHANUTS3/src/Model/PBO11_10116487_LATIHANUTS3.java
@@ -0,0 +1,19 @@
+package Model;
+
+import View.ModelView;
+
+/**
+ *
+ * @author huldan
+ */
+public class PBO11_10116487_LATIHANUTS3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new ModelView().setVisible(true);
+    }
+    
+}
